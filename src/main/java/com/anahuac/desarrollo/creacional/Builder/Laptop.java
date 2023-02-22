175b0ca3454027db01f29bea3d5c2600d4c71710
Kvcc18 <122580911+Kvcc18@users.noreply.github.com>
2023-02-22 16:11:19 -0600

    Add files via upload

--- src/main/java/com/anahuac/desarrollo/creacional/Builder/Laptop.java
+++ src/main/java/com/anahuac/desarrollo/creacional/Builder/Laptop.java
@@ -23,30 +23,37 @@ public class Laptop {
         private String teclado;
         private String os;
 
-        public LaptopBuilder() { }
+        public LaptopBuilder() {
+            System.out.println("Laptop construida");
+        }
 
         public LaptopBuilder ssd (String ssd) {
             this.ssd = ssd;
+            System.out.println("Ssd agregada" + ssd);
             return this;
         }
 
         public  LaptopBuilder ram (String ram) {
             this.ram = ram;
+            System.out.println("Ram agregada" + ram);
             return this;
         }
 
         public LaptopBuilder cpu (String cpu) {
             this.cpu = cpu;
+            System.out.println("CPU agregada" + cpu);
             return this;
         }
 
         public LaptopBuilder teclado (String teclado) {
             this.teclado = teclado;
+            System.out.println("Teclado agregado" + teclado);
             return this;
         }
 
         public LaptopBuilder os (String os) {
             this.os = os;
+            System.out.println("OS agregada" + os);
             return this;
         }
 
